300f8be0b045ffd99b7911d3b895a293cdd3763d
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-31 09:18:28 +0500

    task04 done

---
+++ task04.py
@@ -0,0 +1,11 @@
+ismlar = []
+
+ism1 = input("1-ismni kiriting: ")
+ism2 = input("2-ismni kiriting: ")
+ism3 = input("3-ismni kiriting: ")
+
+ismlar.append(ism1)
+ismlar.append(ism2)
+ismlar.append(ism3)
+
+print(ismlar)
