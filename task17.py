6b3745ede119659c12081cb53a691f29d5c67695
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-31 11:59:33 +0500

    task17 done

---
+++ task17.py
@@ -0,0 +1,13 @@
+name = []
+i = 1
+while True:
+    ism = input(f'{i} - ismim kiriting: ')
+    if ism =="":
+
+        break
+    name.append(ism)
+    i +=1
+    
+    
+print("Ro'yxatdagi ismlar soni:", len(name))
+
