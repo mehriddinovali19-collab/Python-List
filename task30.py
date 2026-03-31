6c7ffd6fc551cdb844a36367c823675979d5161c
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-31 14:44:15 +0500

    task30 done

--- task30.py
+++ task30.py
@@ -1,4 +1,15 @@
 words = []
 for i in range(5):
-    n = input("Palindrom so'z kiriting: ")
+    n = input(f"{i+1} - Palindrom so'z kiriting: ")
     words.append(n)
+
+
+
+new = []
+for soz in words: 
+    if soz[::-1] == soz:
+
+     new.append(soz)
+
+
+print("Natija: ", new)
