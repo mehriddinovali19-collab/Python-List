7da887b4d12beb15b93e6b38e37d39744d62c81e
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-31 10:26:54 +0500

    task10 done

---
+++ task10.py
@@ -0,0 +1,8 @@
+num = [10, 25, 30, 60, 70, 54]
+
+index = int(input("Qaysi indexni o'zgartmoqchisiz: "))
+new_index = int(input("Qanday qiymatga o'zgarmoqchisiz: "))
+
+num[index] = new_index
+
+print("Yangi ro'yxat:", num)
