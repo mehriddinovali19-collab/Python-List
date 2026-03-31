5cdf1890e52e6fb34a126e2684d2a698d0bff0dc
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-31 23:36:54 +0500

    task36 done

---
+++ task36.py
@@ -0,0 +1,16 @@
+text = []
+i = 1
+while True:
+    n = input(f"{i} - mantni kiriting: ")
+    if n == "":
+        break 
+    text.append(n)
+    i += 1
+
+longest_word = text[0]
+
+for w in text:
+    if len(w) > len(longest_word):
+        longest_word = w
+
+print("Eng uzun so'z:", longest_word)
